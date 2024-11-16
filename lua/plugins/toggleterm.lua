1a9ae47a29e6e5daf49950856606703699606a00
rush1016 <russelmicotamayo@gmail.com>
2024-11-16 09:28:59 +0800

    resized toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,8 +5,13 @@ return {
         local highlights = require('rose-pine.plugins.toggleterm')
         require('toggleterm').setup({ 
             open_mapping = [[<c-\>]],
-            highlights = highlights,
-            direction = 'float'
+            -- highlights = highlights,
+            direction = 'float',
+            float_opts = {
+                border = 'single',
+                width = 120,
+                height = 25,
+            }
         })
     end
 }
