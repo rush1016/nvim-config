6d94097d327556f5fcde0092eaa9bc35258a55a9
rush1016 <russelmicotamayo@gmail.com>
2025-02-17 18:52:44 +0800

    added: git blame plugin

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,18 @@
+return {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+        enabled = true,
+        message_template = " <summary> • <date> • <author> • <<sha>>",
+        date_format = "%m-%d-%Y %H:%M", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
+    config = function()
+
+        vim.keymap.set('n', '<leader>goc', ':GitBlameOpenCommitURL<cr>', { silent = true})
+        vim.keymap.set('n', '<leader>gof', ':GitBlameOpenFileURL<cr>', { silent = true})
+        vim.keymap.set('n', '<leader>gcc', ':GitBlameCopyCommitURL<cr>', { silent = true})
+        vim.keymap.set('n', '<leader>gcf', ':GitBlameCopyFileURL<cr>', { silent = true})
+        vim.keymap.set('n', '<leader>gcs', ':GitBlameCopySHA<cr>', { silent = true})
+    end
+}
