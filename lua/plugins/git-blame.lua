5b0d91f1efaedbaf96ca437f0fe3d1fc07a0b828
rush1016 <russelmicotamayo@gmail.com>
2025-02-20 12:11:08 +0800

    deleted: unused keymaps from gitblame

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -9,10 +9,6 @@ return {
     },
     config = function(_, opts)
         require('gitblame').setup(opts)
-        vim.keymap.set('n', '<leader>goc', ':GitBlameOpenCommitURL<cr>', { silent = true, desc = 'Open Commit URL' })
-        vim.keymap.set('n', '<leader>gof', ':GitBlameOpenFileURL<cr>', { silent = true, desc = 'Open File URL' })
-        vim.keymap.set('n', '<leader>gcc', ':GitBlameCopyCommitURL<cr>', { silent = true, desc = 'Copy Commit URL' })
-        vim.keymap.set('n', '<leader>gcf', ':GitBlameCopyFileURL<cr>', { silent = true, desc = 'Copy File URL' })
         vim.keymap.set('n', '<leader>gcs', ':GitBlameCopySHA<cr>', { silent = true, desc = 'Copy commit hash' })
     end
 }
