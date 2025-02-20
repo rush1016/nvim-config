b5495914c40606cd9f67864492bbc14a95441184
rush1016 <russelmicotamayo@gmail.com>
2025-02-20 12:10:52 +0800

    updated: changed mini.pairs to nvim-autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,5 @@
 return {
     {
-        -- Autoclose pairs
-        'echasnovski/mini.pairs',
-        version = '*',
-        config = function()
-            require('mini.pairs').setup()
-        end
-    },
-    {
         -- Better selection of closed pairs
         'echasnovski/mini.ai',
         version = '*',
