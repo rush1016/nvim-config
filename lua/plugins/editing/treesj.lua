6370ba981dc82ea8d0481f501c4f3aedae3b6caf
rush1016 <russelmicotamayo@gmail.com>
2025-04-29 10:55:34 +0800

    updated: treesj to only use leader m/M

--- lua/plugins/editing/treesj.lua
+++ lua/plugins/editing/treesj.lua
@@ -3,6 +3,15 @@ return {
     keys = { '<space>m' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
     config = function()
-        require('treesj').setup()
+        require('treesj').setup({
+            use_default_keymaps = false
+        })
+
+        -- For default preset
+        vim.keymap.set('n', '<leader>m', require('treesj').toggle)
+        -- For extending default preset with `recursive = true`
+        vim.keymap.set('n', '<leader>M', function()
+            require('treesj').toggle({ split = { recursive = true } })
+        end)
     end,
 }
