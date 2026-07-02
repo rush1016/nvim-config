a4cc2512e0fce94b63f86907d75bed6ff32442d9
rush1016 <russelmicotamayo@gmail.com>
2026-07-02 15:16:13 +0800

    debugprint keybinds

--- lua/plugins/others/debugprint.lua
+++ lua/plugins/others/debugprint.lua
@@ -3,12 +3,13 @@ return {
 
     -- opts = { … },
 
-    dependencies = {
-        "echasnovski/mini.nvim" -- Needed for :ToggleCommentDebugPrints (not needed for NeoVim 0.10+)
-    },
-
     version = "*", -- Remove if you DON'T want to use the stable version
     config = function()
         require("debugprint").setup()
+
+        vim.keymap.set('n', '<leader>dd', ":Debugprint delete<CR>", { silent = true })
+        vim.keymap.set('n', '<leader>df', ":Debugprint search<CR>", { silent = true })
+        vim.keymap.set('n', '<leader>dc', ":Debugprint commenttoggle<CR>", { silent = true })
+        vim.keymap.set('n', '<leader>dr', ":Debugprint resetcounter<CR>", { silent = true })
     end,
 }
